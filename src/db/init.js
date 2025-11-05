885d2c7371b3a68f1d9cdd88878140591a72ea49
Elusive Sky <74522772+JoshuaAubryM@users.noreply.github.com>
2025-11-04 17:00:08 -0800

    dockerignore

--- src/db/init.js
+++ src/db/init.js
@@ -3,7 +3,7 @@ dotenv.config({ path: ".env.prod" });
 import mongoose from "mongoose";
 
 export function initDatabase() {
-  const DATABASE_URL = "mongodb://localhost:27017/chat";
+  //   const DATABASE_URL = "mongodb://localhost:27017/chat";
   const PROD_DB = process.env.PROD_DB;
   mongoose.connection.on("open", () => {
     console.info("Successfully connected to database:", PROD_DB);
