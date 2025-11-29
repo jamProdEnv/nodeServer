aa4c7e81802d3fd39739f04e680392b70adce157
Elusive Sky <74522772+JoshuaAubryM@users.noreply.github.com>
2025-11-28 20:59:26 -0800

    Production Environment

--- src/db/init.js
+++ src/db/init.js
@@ -1,12 +1,11 @@
 import dotenv from "dotenv";
-dotenv.config({ path: ".env.prod" });
 dotenv.config();
-dotenv.config({ path: ".env.local" });
+
 import mongoose from "mongoose";
 
 export function initDatabase() {
   // const DATABASE_URL = "mongodb://localhost:27017/chat";
-  const DATABASE_URL = process.env.LOCAL_URL;
+  const DATABASE_URL = process.env.DATABASE_URL;
   // const PROD_DB = process.env.PROD_DB;
   mongoose.connection.on("open", () => {
     console.info("Successfully connected to database:", DATABASE_URL);
